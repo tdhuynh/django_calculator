926404186e11f0d1f0d2d96c538c94d3cf67ae2a
tdhuynh <tommyhuynh93@gmail.com>
2016-10-11 17:44:41 -0400

    calculator working

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 
 
+def operation(num1, num2, sign):
+    if sign == 'add':
+        return num1 + num2
+    elif sign == 'sub':
+        return num1 - num2
+    elif sign == 'mult':
+        return num1 * num2
+    else:
+        return num1 / num2
+
+
 def calculator_view(request):
-    print(request.GET)
-    # num1 = request.GET['num1']
-    # num2 = request.POST['num2']
-    return render(request, 'calculator.html')
+    if request.GET:
+        if request.GET != "" and request.GET == int:
+            num1 = int(request.GET['num1'])
+            num2 = int(request.GET['num2'])
+            sign = request.GET['sign']
+            print(request.GET)
+        else:
+            num1 = 0
+            num2 = 0
+            sign = 'add'
+
+    context = {
+        "solution": operation(num1, num2, sign),
+    }
+    return render(request, 'calculator.html', context)
