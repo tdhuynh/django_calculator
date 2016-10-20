5a8f37bc9bedc56615d80864866837f0ed890f44
tdhuynh <tommyhuynh93@gmail.com>
2016-10-20 00:39:55 -0400

    added css and fixed bugs

--- calculator_app/models.py
+++ calculator_app/models.py
@@ -25,7 +25,10 @@ class Operation(models.Model):
         elif sign == '*':
             return num1 * num2
         else:
-            return num1 / num2
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Can't do that."
 
 USER_TYPE = [
     ('n', 'Normal User'),
